43c2ef52db963ab90e5e91658a6dbf95c12dc8e7
Edyan Cruz <edyan.cruz@upr.edu>
2023-02-03 21:53:13 -0400

    Thoroughly document every bit of code

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # Controls System Data Filter
-Merges the velocity, orientation, position, and pressure data from the IMU, DVL, and pressure sensor into a buffer that is published at a continuous rate.
+Merges the velocity, orientation, position, and pressure data from the IMU, DVL, and pressure sensor into a buffer message that is then published at a continuous rate.
+
+Tested on Ubuntu 18.04.6 LTS with ROS Melodic Morenia, and depends on the [uuv_simulator](https://github.com/uuvsimulator/uuv_simulator) and [rexrov2](https://github.com/uuvsimulator/rexrov2) packages
 
 Project built as a part of training for RUMarino.
+
+### Usage
+
+To run the project, simply clone this repository into the Catkin workspace `src` folder containing both the uuv_simulator and rexrov2 packages. Then run the following commands in order while inside the workspace root directory:
+
+```
+catkin_make
+source devel/setup.bash
+roslaunch controls_system_data_filter cs.launch
+```
+
+This will launch the ocean_waves environment in Gazebo, spawn a RexROV 2 UUV in the simulation, and finally start the controls system data filter, which will begin publishing the buffer message and also log it to the console at the same time.
+
+
+
+
--- scripts/controls_system_node.py
+++ scripts/controls_system_node.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
+"""
+Controls System Data Filter
+---------------------------
+Merges the velocity, orientation, position, and pressure data
+from the IMU, DVL, and pressure sensor into a buffer message that is then 
+published at a continuous rate.
+
+ROS Parameters
+--------------
+CS_TOPIC_NAME : str
+CS_NODE_NAME : str
+CS_BEAM_INDEX : int
+CS_PUB_INTERVAL : int
+CS_QUEUE_SIZE : int
+
+ROS Subscribed Topics
+---------------
+/cs/imu : sensor_msgs/Imu
+/cs/dvl : uuv_sensor_ros_plugins_msgs/DVL
+/cs/pressure : sensor_msgs/FluidPressure
+
+ROS Published Topics
+--------------
+CS_TOPIC_NAME : controls_system/Buffer
+"""
 import rospy
 from sensor_msgs.msg import Imu, FluidPressure
 from uuv_sensor_ros_plugins_msgs.msg import DVL
@@ -9,11 +34,17 @@ _TOPIC_NAME = rospy.get_param('CS_TOPIC_NAME')
 
 class ControlsSystemDataFilter:
     """
-    Controls System Data Filter
-    ----------------------
-    Merges the velocity, orientation, position, and pressure data
-    from the IMU, DVL, and pressure sensor into a buffer that is published at
-    a continuous rate.
+    Controls System Data Filter Class
+    ---------------------------------
+    
+    Methods
+    -------
+    __init__(TOPIC_NAME, NODE_NAME)
+        Initializes the node, subscribes to the topics, and publishes the buffer
+    add_to_buffer(msg)
+        Adds the corresponding data to the buffer per message type
+    publish_to_topic(event)
+        Publishes the given buffer
     """
     def __init__(self, TOPIC_NAME, NODE_NAME):
         """
@@ -22,17 +53,22 @@ class ControlsSystemDataFilter:
         Initializes the node using rospy, creates the buffer message,
         subscribes to the IMU, DVL, and pressure topics, and publishes the
         buffer at a continuous rate.
+        
+        Parameters
+        ----------
+        TOPIC_NAME : str
+        NODE_NAME : str 
         """
         rospy.init_node(NODE_NAME, anonymous=True)
 
-        # Subscribe to each of the topics to filter through
+        # Create the buffer message and subscribe to the topics
         self.buffer = Buffer()
         self.beamindex = rospy.get_param('CS_BEAM_INDEX')
         rospy.Subscriber('/cs/imu', Imu, self.add_to_buffer)
         rospy.Subscriber('/cs/dvl', DVL, self.add_to_buffer)
         rospy.Subscriber('/cs/pressure', FluidPressure, self.add_to_buffer)
 
-        # Publish the buffer at the specified interval
+        # Publish the buffer at the specified interval and queue size
         interval = rospy.get_param('CS_PUB_INTERVAL')
         q_size = rospy.get_param('CS_QUEUE_SIZE')
         timer = rospy.Timer(rospy.Duration(interval), self.publish_to_topic)
@@ -41,13 +77,16 @@ class ControlsSystemDataFilter:
         rospy.spin()
         timer.shutdown()
 
-    # Adds the corresponding data to the buffer per message type
     def add_to_buffer(self, msg):
         """
         Add to Buffer Method
         --------------------
         Filters through the given message types, then sets the data
         that is needed for the buffer message.
+        
+        Parameters
+        ----------
+        msg : sensor_msgs/Imu, uuv_sensor_ros_plugins_msgs/DVL, sensor_msgs/FluidPressure
         """
         if msg._type == 'sensor_msgs/Imu':
             self.buffer.angular_velocity = msg.angular_velocity
@@ -58,13 +97,16 @@ class ControlsSystemDataFilter:
         elif msg._type == 'sensor_msgs/FluidPressure':
             self.buffer.fluid_pressure = msg.fluid_pressure
 
-    # Publishes the given buffer
     def publish_to_topic(self, event):
         """
         Publish to Topic Method
         -----------------------
-        Publishes the current buffer to the controls_system_topic
+        Publishes the current buffer message to the CS_TOPIC_NAME
         topic and logs the contents of it.
+        
+        Parameter
+        ---------
+        event : rospy.TimerEvent
         """
         self.pub.publish(self.buffer)
         rospy.loginfo(self.buffer)
